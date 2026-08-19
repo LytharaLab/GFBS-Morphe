913112c807e64dd031bdf07a5f7575ac0f66a943
Convex89524 <1527824288@qq.com>
2026-08-19 17:48:28 +0800

    upd-1.0.1

--- src/main/java/org/lytharalab/gfbs/morphe/api/Morphe.java
+++ src/main/java/org/lytharalab/gfbs/morphe/api/Morphe.java
@@ -1,6 +1,7 @@
 package org.lytharalab.gfbs.morphe.api;
 
 import net.minecraft.resources.ResourceLocation;
+import org.lytharalab.gfbs.morphe.MorpheBuildInfo;
 import org.lytharalab.gfbs.morphe.core.UiDocument;
 import org.lytharalab.gfbs.morphe.core.UiElement;
 import org.lytharalab.gfbs.morphe.widget.ButtonElement;
@@ -23,7 +24,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public final class Morphe {
     public static final String MOD_ID = "gfbs_morphe";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = MorpheBuildInfo.VERSION;
     public static final int SCRIPT_API_VERSION = 1;
 
     private static final Morphe INSTANCE = new Morphe();
